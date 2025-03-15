f24cae40e8bc3afab1e1ac4a2198ad621ec6be94
Rock Boynton <rock.boynton@yahoo.com>
2025-03-15 11:06:27 -0700

    logiops working

--- logiops.nix
+++ logiops.nix
@@ -12,14 +12,10 @@
       Type = "simple";
       ExecStart = "${pkgs.logiops_0_2_3}/bin/logid";
       User = "root";
+      Restart = "on-failure";
+      RestartSec = "5s";
     };
   };
-
-  # Add a `udev` rule to restart `logiops` when the mouse is connected
-  # https://github.com/PixlOne/logiops/issues/239#issuecomment-1044122412
-  services.udev.extraRules = ''
-    ACTION=="change", SUBSYSTEM=="power_supply", ATTRS{manufacturer}=="Logitech", ATTRS{model_name}=="Wireless Mouse MX Master 3", RUN{program}="${pkgs.systemd}/bin/systemctl --no-block try-restart logiops.service"
-  '';
 
   # Configuration for logiops
   environment.etc."logid.cfg".text = /* json */ ''
@@ -43,14 +39,14 @@
             mode: "OnRelease";
             action: {
               type: "Keypress";
-              keys: ["KEY_F15"];
+              keys: ["KEY_LEFTCTRL", "KEY_C"];
             };
           }, {
             direction: "Right";
             mode: "OnRelease";
             action: {
               type: "Keypress";
-              keys: ["KEY_F16"];
+              keys: ["KEY_LEFTCTRL", "KEY_V"];
             };
           }, {
             direction: "Down";
@@ -71,7 +67,7 @@
             mode: "OnRelease";
             action: {
               type: "Keypress";
-              keys: ["KEY_PLAYPAUSE"];
+              keys: ["KEY_ENTER"];
             };
           });
         };
