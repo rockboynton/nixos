fe186b0ba577edcec1737cbc05fc1c49fd63c6a2
Rock Boynton <rboynton@anduril.com>
2026-09-10 01:16:47 -0700

    override csv highlighting to fix gruvbox

---
+++ helix/runtime/queries/csv/highlights.scm
@@ -0,0 +1,14 @@
+(first)@type
+(second)@function
+(third)@variable.other.member
+(fourth)@operator
+(fifth)@type
+(sixth)@function
+(seventh)@variable.other.member
+
+[
+  ";"
+  ","
+  "|"
+] @punctuation.delimiter
+
